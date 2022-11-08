ba0eeb2021f0a1aacd124d9448afefa7c8fd19a7
kriviacom <108778833+kriviacom@users.noreply.github.com>
2022-11-08 20:49:36 -0300

    setDatabase

--- consortium/consortium.go
+++ consortium/consortium.go
@@ -2,9 +2,30 @@ package consortium
 
 import (
         "github.com/gofiber/fiber"
+        _ "github.com/go-sql-driver/mysql"
+        "database/sql"
+        "fmt"
 )
 
-func GetConsortiums(db *sql.DB, c *fiber.Ctx) {
+var db *sql.DB
+
+func setDatabase(s string)
+{
+        var err error
+        db, err = sql.Open("mysql", s)
+        var v = "Não conseguiu conectar ao banco de dados"
+        
+        if err != nil {
+                fmt.Println(v)
+                panic(err)
+                }
+
+        fmt.Println("conexão OK!")
+        fmt.Println(db)
+        
+}
+
+func GetConsortiums(c *fiber.Ctx) {
         c.SendString("All Books x")
 }
 
