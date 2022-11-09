e99491a92bce1f8e7fa1bb9d7f58fbc039c59211
kriviacom <108778833+kriviacom@users.noreply.github.com>
2022-11-08 23:15:33 -0300

    Update consortium.go

--- consortium/consortium.go
+++ consortium/consortium.go
@@ -8,6 +8,25 @@ import (
 
 func GetConsortiums(c *fiber.Ctx, db *sql.DB) {
         c.SendString("All Books x")
+        
+	rows, err := db.Query("SELECT name FROM consortium")
+	if err != nil {
+		panic(err)
+		}
+
+	for rows.Next() {
+		var name string
+
+		/* Leemos el registro */
+		rows.Scan(&name)
+
+		/* Escribimos el tí­tulo por pantalla */
+		fmt.Println( "Post title: "+name )
+		}
+	}
+        
+        
+        
 }
 
 func GetConsortium(c *fiber.Ctx) {
